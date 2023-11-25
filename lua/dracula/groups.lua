5de7093a7aeb8f92a462a98074414183886f7b15
Steeve Lefort <steeve@lefort-software.com>
2023-11-25 14:03:15 +0100

    test color

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -237,6 +237,15 @@ local function setup(configs)
       ['@neorg.links.file'] = { fg = colors.fg },
       ['@neorg.links.location'] = { fg = colors.cyan },
 
+      ['@neorg.todo_items.cancelled'] = { fg = colors.pink },
+      ['@neorg.todo_items.done'] = { fg = colors.pink },
+      ['@neorg.todo_items.on_hold'] = { fg = colors.pink },
+      ['@neorg.todo_items.pending'] = { fg = colors.pink },
+      ['@neorg.todo_items.recurring'] = { fg = colors.pink },
+      ['@neorg.todo_items.uncertain'] = { fg = colors.pink },
+      ['@neorg.todo_items.undone'] = { fg = colors.pink },
+      ['@neorg.todo_items.urgent'] = { fg = colors.pink },
+
       -- HTML
       htmlArg = { fg = colors.green, },
       htmlBold = { fg = colors.yellow, bold = true, },
