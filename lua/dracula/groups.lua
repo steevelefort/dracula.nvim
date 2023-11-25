8e853e8d82a4442f4a1ddea830c89791acb28a41
Steeve Lefort <steeve@lefort-software.com>
2023-11-25 17:32:13 +0100

    test color

--- lua/dracula/groups.lua
+++ lua/dracula/groups.lua
@@ -250,6 +250,8 @@ local function setup(configs)
 
       ['@neorg.code'] = { fg = colors.purple },
       ['@neorg.markup.code'] = { fg = colors.purple },
+      ['@neorg.markup'] = { fg = colors.purple },
+      ['@neorg.markups'] = { fg = colors.purple },
       ['@neorg.tags.ranged_verbatim.code_block'] = { fg = colors.purple },
       ['@neorg.ranged_verbatim_tag'] = { fg = colors.purple },
       ['@neorg.tags'] = { fg = colors.purple },
